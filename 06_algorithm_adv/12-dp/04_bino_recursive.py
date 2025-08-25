befae032a3eb11c01964909056c3e75491e6ef7d
hwimin <whimin0319@gmail.com>
2025-08-25 11:04:29 +0900

    fibo_recursive updated

---
+++ 06_algorithm_adv/12-dp/04_bino_recursive.py
@@ -0,0 +1,6 @@
+def bino(n, k):
+    pass
+
+n = 5
+k = 2
+print(bino(n, k))  # 출력: 10
---
+++ 06_algorithm_adv/12-dp/06_coin_change.py
@@ -0,0 +1,7 @@
+def coin_change(coins, amount):
+    pass
+
+coins = [1, 4, 6]  # 사용 가능한 동전의 종류
+amount = 8  # 만들어야 할 금액
+
+print(coin_change(coins, amount))
