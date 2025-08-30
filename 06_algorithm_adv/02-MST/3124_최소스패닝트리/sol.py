969e61d479ff3f2c40501f2b245d48ea8ab2f833
hwi-min <whimin0319@gmail.com>
2025-08-30 15:45:15 +0900

    swea 3124 solved

--- 06_algorithm_adv/02-MST/3124_최소스패닝트리/sol.py
+++ 06_algorithm_adv/02-MST/3124_최소스패닝트리/sol.py
@@ -9,16 +9,26 @@ def find_set(x):
     return parents[x]
 
 def union(x, y):
-    rep_x, rep_y = map
+    rep_x, rep_y = find_set(x), find_set(y)
+    if rep_x != rep_y:
+        if ranks[rep_x] > ranks[rep_y]:
+            parents[rep_y] = rep_x
+        elif ranks[rep_x] < ranks[rep_y]:
+            parents[rep_x] = rep_y
+        else:
+            parents[rep_y] = rep_x
+            ranks[rep_x] += 1
+        return True
+    return False
 
 T = int(input())
-for _ in range(1, T+1):
+for t in range(1, T+1):
     V, E = map(int, input().split())
     parents = [i for i in range(V+1)]
     ranks = [0 for _ in range(V+1)]
     visited = set()
     edges = []
-    min_sum = 1000000 * E # 최악의 경우
+    min_sum = 0
 
     for _ in range(E):
         n1, n2, w = map(int, input().split()) # w: 음수일수도 있다 !
@@ -28,7 +38,9 @@ for _ in range(1, T+1):
     edges.sort(key=lambda x: x[2])
 
     for n1, n2, w in edges:
-        union(n1, n2)
+        if union(n1, n2):
+            min_sum += w
+    print(f"#{t} {min_sum}")
 
 
 
